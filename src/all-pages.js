23a97f8f631e9e29206d3af130e3f3cc7181a289
Paméla <pamela.schmidinger@zhdk.ch>
2021-06-23 10:57:31 +0200

    added app css class to page on all pages

--- src/all-pages.js
+++ src/all-pages.js
@@ -17,7 +17,7 @@ export default function AllPages(props) {
       onLoadSuccess={onDocumentLoadSuccess}
     >
       {Array.from(new Array(numPages), (el, index) => (
-        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+        <Page container key={`page_${index + 1}`} pageNumber={index + 1} />
       ))}
     </Document>
   );
