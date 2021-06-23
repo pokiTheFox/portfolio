bd204e886806348e09c36e6616d9a4eb5ad0c867
Paméla <pamela.schmidinger@zhdk.ch>
2021-06-23 12:05:39 +0200

    tried stylesheet again

--- src/all-pages.js
+++ src/all-pages.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Document, Page } from "react-pdf";
+import { Document, Page, StyleSheet  } from "react-pdf";
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+});
 
 
 export default function AllPages(props) {
@@ -18,7 +26,7 @@ export default function AllPages(props) {
       onLoadSuccess={onDocumentLoadSuccess}
     >
       {Array.from(new Array(numPages), (el, index) => (
-        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+        <Page key={`page_${index + 1}`} pageNumber={index + 1} style={styles.page}/>
       ))}
     </Document>
   );
