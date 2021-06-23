721ac739f9f97a45f87b11aefe8951d09694997d
Paméla <pamela.schmidinger@zhdk.ch>
2021-06-23 09:54:25 +0200

    added css to make pdf center of page

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
   
   render() {
     return (
-      <div className="App">
+      <div className="App container" >
 
       <div className="all-page-container">
         <AllPagesPDFViewer pdf={samplePDF} />
